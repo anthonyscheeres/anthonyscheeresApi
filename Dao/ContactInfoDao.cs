4d068b5a3a0410d451a8770326ea7387dca5e04c
Anthony <info@anthonyscheeres.nl>
2020-03-23 17:30:23 +0100

    .

--- Dao/ContactInfoDao.cs
+++ Dao/ContactInfoDao.cs
@@ -36,7 +36,7 @@ namespace ChantemerleApi.Dao
         internal void changeContactInfoByModelInDatabase(ContactInfoModel contactInfo)
         {
             //const query for updating each record of the table
-            const string sqlQueryForChangingContactInfo = "update contact_information_owner set house_nickname = @house_nickname; update contact_information_owner set place = @place; update contact_information_owner set address = @address; update contact_information_owner set postal_code = @postal_code; update contact_information_owner set family_name = @family_name; update contact_information_owner set telephone = @telephone; update contact_information_owner set mail = @mail";
+            const string sqlQueryForChangingContactInfo = "update contact_information_owner set name = @name; update contact_information_owner set place = @place; update contact_information_owner set address = @address; update contact_information_owner set postal_code = @postal_code; update contact_information_owner set telephone = @telephone; update contact_information_owner set mail = @mail";
 
             using var connectionWithDatabase = new NpgsqlConnection(cs); //start new Npgsql instance for connecting with an postgres database//start new Npgsql instance for connecting with an postgres database
             connectionWithDatabase.Open(); //open the connection
@@ -52,11 +52,10 @@ namespace ChantemerleApi.Dao
 
 
             //Insert variables in prepared statment
-            command.Parameters.AddWithValue("@house_nickname", contactInfo.house_nickname == null ? (object)DBNull.Value : contactInfo.house_nickname);
+            command.Parameters.AddWithValue("@name", contactInfo.name == null ? (object)DBNull.Value : contactInfo.name);
             command.Parameters.AddWithValue("@place", contactInfo.place ==null ? (object)DBNull.Value : contactInfo.place);
             command.Parameters.AddWithValue("@address", contactInfo.address==null ? (object)DBNull.Value : contactInfo.address);
             command.Parameters.AddWithValue("@postal_code", contactInfo.postal_code ==null ? (object)DBNull.Value : contactInfo.postal_code);
-            command.Parameters.AddWithValue("@family_name", contactInfo.family_name == null ? (object)DBNull.Value : contactInfo.family_name);
             command.Parameters.AddWithValue("@telephone", contactInfo.telephone ==null ? (object)DBNull.Value : contactInfo.telephone);
             command.Parameters.AddWithValue("@mail", contactInfo.mail ==null ? (object)DBNull.Value : contactInfo.mail);
 
--- Models/ContactInfoModel.cs
+++ Models/ContactInfoModel.cs
@@ -7,12 +7,11 @@ namespace ChantemerleApi.Models
 {
     public class ContactInfoModel
     {
-        public string? house_nickname { get; set; }
+        public string? name { get; set; }
         public string? place { get; set; }
         public string? address { get; set; }
         public string? postal_code { get; set; }
 
-        public string? family_name { get; set; }
         public string? telephone { get; set; }
         public string? mail { get; set; }
 
